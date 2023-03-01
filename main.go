d926ec830ba0fff847ee24b086e099e26a757fbd
igorGevaerd <igor.gevaerd@gmail.com>
2023-03-01 20:57:23 -0300

    added main for initial tests

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
+)
 
+func main() {
+	svc := NewDogFactService("https://dogapi.dog/api/v2/facts?limit=1")
+	svc = NewLoggingService(svc)
+
+	fact, err := svc.GetDogFact(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n", fact)
+	fmt.Println("xisd")
 }
