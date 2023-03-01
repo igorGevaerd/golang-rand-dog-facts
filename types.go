c9f5d3324fb8d4c283fe51d000f047d8ba1f4e75
igorGevaerd <igor.gevaerd@gmail.com>
2023-03-01 20:53:52 -0300

    fixed struct based on json output

--- types.go
+++ types.go
@@ -1,5 +1,28 @@
 package main
 
 type DogFact struct {
-	Fact string `json:"fact"`
+	Fact string `json:"body"`
 }
+
+type DogInfo struct {
+	Id         string  `json:"id"`
+	Type       string  `json:"type"`
+	Attributes DogFact `json:"attributes"`
+}
+
+type DogData struct {
+	Data []DogInfo `json:"data"`
+}
+
+// response example:
+// {
+// 	"data":[
+// 		 {
+// 				"id":"591a1d34-475e-4a8d-862a-baeca4f2b19c",
+// 				"type":"fact",
+// 				"attributes":{
+// 					 "body":"Most dogs have 18 or more muscles to tilt, rotate, and move their ears."
+// 				}
+// 		 }
+// 	]
+// }
